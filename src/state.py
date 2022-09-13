38cf2a7fe64c992fd159c90810d33c2b6a3a984f
Shihao Cao <shehowcow@gmail.com>
2022-09-13 12:36:33 -0400

    fill out some of state

--- src/state.py
+++ src/state.py
@@ -1,23 +1,34 @@
+from collections import defaultdict
 from typing import Dict, List
 
+class DataPoint:
+    time: int
+    data: int
+    def __init__(self, time: int, digit: int):
+        self.time = time
+        self.digit = digit
+    
 
 class State:
 
-    ip_map: Dict[str, int]
+    ip_map: Dict[str, Dict[int, DataPoint]]
 
     def __init__(self):
-        self.ip_map = map()
+        self.ip_map = defaultdict({})
 
-    def add_node(ip: str, port: int):
+    def add_node(self, ip: str, port: int):
+        self.ip_map[ip][port] = None
         pass
 
-    def add_node_with_data(ip: str, port: int, time: int, digit: int):
+    def add_node_with_data(self, ip: str, port: int, time: int, digit: int):
+        self.ip_map[ip][port] = DataPoint(time, digit)
         pass
 
-    def update_node(ip: str, port: int, time: int, digit: int):
+    def update_node(self, ip: str, port: int, time: int, digit: int):
+        self.ip_map[ip][port] = DataPoint(time, digit)
         pass
 
-    def get_random_ip() -> str:
+    def get_random_ip(self) -> str:
         pass
 
     def get_state() -> List[str]:
