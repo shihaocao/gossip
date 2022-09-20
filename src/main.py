d1a6719fb0e1a4ed522b85877ca8bffa877db30b
jack-farley <jfarley872@gmail.com>
2022-09-20 12:01:00 -0400

    changes

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def tcp_listener(state: State, local_port: int) -> None:
         connection.close()
 
 
-def gossip(state: State, local_ip: str, local_port: int, ip: str) -> None:
+def gossip(state: State, local_port: int, ip: str) -> None:
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind((socket.gethostname(), local_port))
 
@@ -37,10 +37,10 @@ def gossip(state: State, local_ip: str, local_port: int, ip: str) -> None:
         state.update_node(node_ip, node_port, time, digit)
 
 
-def gossiper(state: State, local_ip: str, local_port: int) -> None:
+def gossiper(state: State, local_port: int) -> None:
     target_ip = state.get_random_ip()
     if target_ip != None:
-        gossip(state, local_ip, local_port, target_ip)
+        gossip(state, local_port, target_ip)
 
     # run the gossiper again in 3 seconds
     threading.Timer(3, gossiper, args=(state, local_port)).start()
@@ -48,18 +48,18 @@ def gossiper(state: State, local_ip: str, local_port: int) -> None:
 
 def terminal_listener(state: State, local_ip: str, local_port: int) -> None:
     while True:
-        input: str = input(">> ")
-        if input == "?":
+        input_val: str = input(">> ")
+        if input_val == "?":
             for state_value in state.get_state():
                 print(state_value)
 
-        elif len(input) == 1 and input.isdigit():
+        elif len(input_val) == 1 and input_val.isdigit():
             state.update_node(
-                local_ip, port, datetime.now().timestamp(), int(input))
+                local_ip, port, datetime.now().timestamp(), int(input_val))
 
-        elif input.startswith("+"):
+        elif input_val.startswith("+"):
             # TODO add validation
-            ip_and_port: List[str] = input[1:].split(':')
+            ip_and_port: List[str] = input_val[1:].split(':')
             ip = ip_and_port[0]
             port = int(ip_and_port[1])
 
@@ -75,12 +75,12 @@ def main():
 
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    local_port: int = int(sys.argv[1])
     local_ip: str = socket.gethostname()
+    local_port: int = int(sys.argv[1])
 
     serversocket.close()
 
-    state: State = State()
+    state: State = State(local_ip, local_port)
 
     thread1 = threading.Thread(target=tcp_listener, args=(state, local_port))
     thread2 = threading.Thread(target=gossiper, args=(state, local_port))
@@ -90,6 +90,10 @@ def main():
     thread1.start()
     thread2.start()
     thread3.start()
+
+    thread1.join()
+    thread2.join()
+    thread3.join()
 
 
 if __name__ == "__main__":
