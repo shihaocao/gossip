84c96c3302e569681e40d8df05ead8ec616d71a0
jack-farley <jfarley872@gmail.com>
2022-09-20 11:57:43 -0400

    changes

--- src/state.py
+++ src/state.py
@@ -8,17 +8,19 @@ import threading
 
 random.seed(123)
 
+
 class DataPoint:
     data_time: int
     digit: int
+
     def __init__(self, data_time: int, digit: int):
         self.data_time = data_time
         self.digit = digit
-        
+
     def __eq__(self, other):
         if not isinstance(other, DataPoint):
             return NotImplemented
-        
+
         return (
             self.data_time == other.data_time
             and self.digit == other.digit
@@ -29,6 +31,7 @@ class DataPoint:
 
     def __repr__(self) -> str:
         return str(self)
+
 
 class State:
 
@@ -41,17 +44,17 @@ class State:
         self.lock = threading.Lock()
 
     def update_node(self, ip: str, port: int, data_time: int, digit: int):
-        
+
         if data_time > int(time.time()):
             # time is in the future, reject
             return
-        
+
         with self.lock:
-        
+
             if (ip, port) in self.banned_set:
                 # reject if banned
                 return
-                    
+
             if port in self.ip_map[ip]:
                 existing_data_point = self.ip_map[ip][port]
                 if data_time > existing_data_point.data_time:
@@ -68,52 +71,56 @@ class State:
 
                     stalest_time = 1700000000
                     stalest_port = 0
-                    
+
                     for port, data_point in port_data_point_pairs:
                         if data_point.data_time < stalest_time:
                             stalest_time = data_point.data_time
                             stalest_port = port
-                    
+
                     self.ip_map[ip].pop(stalest_port)
-        
+
     def add_to_banned_set(self, ip: str, port: int) -> None:
         self.lock.acquire()
-     
+
         self.banned_set.add((ip, port))
-        
+
         # remove from self.ip_map as well
         if ip in self.ip_map and port in self.ip_map[ip]:
             self.ip_map[ip].pop(port)
             if len(self.ip_map[ip]) == 0:
                 self.ip_map.pop(ip)
-                
+
         self.lock.release()
 
         return
 
     def get_random_ip(self) -> Tuple[str, int]:
         self.lock.acquire()
-                
+
+        if len(self.ip_map) == 0:
+            self.lock.release()
+            return None
+
         random_ip = random.choice(list(self.ip_map.keys()))
         random_port = random.choice(list(self.ip_map[random_ip]))
 
         self.lock.release()
 
         return random_ip, random_port
-    
-    def _get_node_data_as_str(self, ip: str, port: int) -> str:      
+
+    def _get_node_data_as_str(self, ip: str, port: int) -> str:
         data_point = self.ip_map[ip][port]
         return f"{ip}:{port},{data_point.data_time},{data_point.digit}"
 
     def get_state(self) -> List[str]:
-        
+
         self.lock.acquire()
-        
+
         return_list = []
         for ip, port_map in self.ip_map.items():
             for port in port_map.keys():
                 return_list.append(self._get_node_data_as_str(ip, port))
 
         self.lock.release()
-        
+
         return return_list
