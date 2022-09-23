ad76b4313fe61b58d71765ca2ce3eb471d39c835
jack-farley <jfarley872@gmail.com>
2022-09-23 18:37:44 -0400

    generate submission

---
+++ generate-submission.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# prepare the zips
+mkdir zips
+rm "zips/submission.zip"
+
+INCLUDED="src test .gitignore LICENSE README.md"
+
+EXCLUDED="src/__pycache__ test/__pycache__"
+
+zip -r "zips/submission.zip" $INCLUDED -x $EXCLUDED
